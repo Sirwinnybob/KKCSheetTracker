c6a1d9fdf6f50a65015585f8a3e256066b480726
Developer <developer@example.com>
2026-06-03 13:50:52 -0700

    Bump version to 4.5.0 and code to 4500

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.kkc.sheettracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4003
-        versionName = "4.0.3"
+        versionCode = 4500
+        versionName = "4.5.0"
     }
 
     signingConfigs {
