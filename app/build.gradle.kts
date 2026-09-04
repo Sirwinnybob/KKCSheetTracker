c56559a067a8d8878e950f6aa95d4aaaf5b67be0
Developer <developer@example.com>
2026-09-03 22:12:06 -0700

    build: bump version to 8.4.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.kkc.sheettracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 80409
-        versionName = "8.4.9"
+        versionCode = 80411
+        versionName = "8.4.11"
     }
 
     signingConfigs {
