0fe3096dfa9202f265080b6937f28b3edfbc53f0
Developer <developer@example.com>
2026-09-22 14:36:24 -0700

    chore: bump version to 8.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.kkc.sheettracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 80501
-        versionName = "8.5.1"
+        versionCode = 80505
+        versionName = "8.5.5"
     }
 
     signingConfigs {
